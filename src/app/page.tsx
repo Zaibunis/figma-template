fbef674e9789de53d517346865b42a51efcb88f8
Zaibunis <zainabmustaqeem123@hotmail.com>
2024-12-09 17:35:38 +0500

    make more changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import Index from "./component/index";
 import Navbar from "./component/navbar";
-import Header from "./component/header";
-import Brand from "./component/brand";
 import Arrival from "./component/arrival";
+import Cart from "./component/cart";
+import Shop from "./component/shop";
+import Main from "./component/main";
+import Home from "./component/home";
 
 export default function Land() {
   return (
    <div className="bg-gray-100">
     <Index />
     <Navbar />
-    <Header />
-    <Brand />
-<Arrival />
+    <Arrival />
+    <Cart />
+    <Shop />
+    <Main />
+    <Home />
    </div>
   );
 }
