e6c0a2f5df38fc6fb749a3a6a055b3a471c27b0b
Zaibunis <zainabmustaqeem123@hotmail.com>
2025-01-21 00:22:43 +0500

    new changes

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,9 +3,10 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId:"017bgzcc",
+  dataset:"production",
+  apiVersion:"v2025-01-07",
   useCdn: false,
-  token:process.env.SANITY_API_TOKEN
+  token:"skR291TJMmpTOtPT0dLebvzc3nGRjEEaPSFXacmMjQiJcDIqrMZEm9bWrUg1OSVxSqt3LvlU2t80eu3OquX3bMSoC50FD8iBZi75XGXJDBflcrxFZnoRFEWiaFFYesJ7CWaPU4E5ufjESJve759zfCzQHROX5xpMkOipczNCvg2tgaYyMB7l"
+
 })
