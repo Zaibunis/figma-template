05f05cbee5438def801a992ced0bbc588422df27
Zaibunis <zainabmustaqeem123@hotmail.com>
2025-01-21 01:59:40 +0500

    new

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -230,7 +230,7 @@ const SecThree = () => {
   <p className="text-white text-sm text-center mb-4 line-clamp-2">
     {product.description}
   </p>
-  <Link href={`/product/${product._id}`}>
+  <Link href={`/productOne/${product._id}`}>
     <button className="bg-[#B88E2F] text-black px-6 py-2 font-semibold hover:bg-[#A67C2A] transition-colors duration-300">
       View Detail
     </button>
